97aac7f46715ab908357b90484f7aa83ed9ae17a
Joel Ringelstetter Jr <jringelstetterjr@yahoo.com>
2024-07-21 22:28:06 -0500

    initial commit

---
+++ backend/steam/src/test/java/com/database/steam/SteamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.database.steam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
