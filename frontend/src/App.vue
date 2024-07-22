2420ef33e71f4f7c85f1ca36957b1698c88e44b0
Joel Ringelstetter Jr <jringelstetterjr@yahoo.com>
2024-07-21 22:33:16 -0500

    cute welcome message for the gang

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Hey Guys! We're gonna go super crazy with this application here!"/>
 </template>
 
 <script>
